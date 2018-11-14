8b917bab917a5fc319efb0a0c3c4a2bb3b8a23ed
Juriel Garcia <jurielgarcia2010@gmail.com>
2018-11-13 22:55:42 -0500

    Music Time!
    Added some music to the UI and Menu

---
+++ TheSimulation/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonController : MonoBehaviour, IPointerEnterHandler {
+
+    public AudioSource source;
+    public AudioClip clip;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        source.PlayOneShot(clip);
+    }
+}
